792fe68c979068defd847c284d27bc1fcc459c78
ninja <elalfy.mohamed19@gmail.com>
2022-07-10 03:48:05 +0200

    #

---
+++ sanity-backend/schemas/comment.js
@@ -0,0 +1,17 @@
+export default {
+  name: "comment",
+  title: "Comment",
+  type: "document",
+  fields: [
+    {
+      name: "postedBy",
+      title: "posted By",
+      type: "string",
+    },
+    {
+      name: "comment",
+      title: "Comment",
+      type: "string",
+    },
+  ],
+};
